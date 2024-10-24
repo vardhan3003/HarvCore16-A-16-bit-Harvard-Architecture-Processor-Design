6e0e2b3fe415a3052edb6f963bc66a19bf3445eb
Harshavardhan Babu <139210570+vardhan3003@users.noreply.github.com>
2024-10-24 21:30:04 +0530

    Update tb.v

--- tb.v
+++ tb.v
@@ -1,6 +1,4 @@
-`timescale 1ns/1ps 
 module tb;
- 
  
 integer i = 0;
  
@@ -8,20 +6,16 @@ reg clk = 0,sys_rst = 0;
 reg [15:0] din = 0;
 wire [15:0] dout;
  
- 
-top dut(clk, sys_rst, din, dout);
+processor dut(clk, sys_rst, din, dout);
  
 always #5 clk = ~clk;
  
 initial begin
-sys_rst = 1'b1;
-repeat(5) @(posedge clk);
-sys_rst = 1'b0;
-#2000;
-$stop;
+    sys_rst = 1'b1;
+    repeat(5) @(posedge clk);
+    sys_rst = 1'b0;
+    #3000;
+    $stop;
 end
  
 endmodule
-
-
-
